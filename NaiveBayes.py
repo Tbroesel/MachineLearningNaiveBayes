3229722c438a7d5b6bd96f1175988ecbee371db0
Tbroesel <tylersbroesel@gmail.com>
2024-09-10 14:17:26 -0600

    Fixed Divide by zero errors that were occuring in NaiveBayes. Performance of algorithim is poor may need some tweaking.

--- NaiveBayes.py
+++ NaiveBayes.py
@@ -14,8 +14,10 @@ class NaiveBayes:
         self._priors = np.zeros(classes_count, dtype=np.float64)
 
         for i, c in enumerate(self._classes):
+            
             X_c = X[y == c]
             self._mean[i, :] = X_c.mean(axis=0)
+
             self._var[i, :] = X_c.var(axis=0)
             self._priors[i] = X_c.shape[0] / float(sample_count)
 
@@ -29,7 +31,7 @@ class NaiveBayes:
         # calculate posterior probability for each class
         for i, c in enumerate(self._classes):
             prior = np.log(self._priors[i])
-            posterior = np.sum(np.log(self._pdf(i, x)))
+            posterior = np.sum(np.log(self._pdf(i, x) + 1e-10))
             posterior = posterior + prior
             posteriors.append(posterior)
 
@@ -38,10 +40,14 @@ class NaiveBayes:
 
     # calculate probability distribution (using gaussian distribution)
     def _pdf(self, class_index, x):
+        epsilon = 1e-10
         mean = self._mean[class_index]
         var = self._var[class_index]
-        numerator = np.exp(-((x - mean) ** 2) / (2 * (var ** 2)))
-        denominator = np.sqrt(2 * np.pi) * var
+        #print(-((x - mean) ** 2))
+        #print(( ((var + epsilon) * 2)))
+        numerator = np.exp(-((x - mean) ** 2) / ( ((var + epsilon) * 2)))
+
+        denominator = np.sqrt(2 * np.pi * (var + epsilon))
         return numerator / denominator
 
     # calculate accuracy of algorithm compared to known results
