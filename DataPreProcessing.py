18b8d22da653fb7e7f2f92b7910ef6025e2d2940
Camden Roberts <camdencornetta@gmail.com>
2024-09-13 19:11:16 -0600

    Added functionality to split data set into training set (with culled data and shuffled positioning) and test set (full dataset). Tuned parameters to increase accuracy

--- DataPreProcessing.py
+++ DataPreProcessing.py
@@ -10,12 +10,18 @@ class processData:
 
         X = pd.DataFrame(dat)
 
-        X.replace(0.00, 0.01, inplace = True)   # replace 0 values
-        X = X.sample(frac = 1)                                  # shuffle the data
-        n = round(int(len(X) * .20))                             # get size of training dataset
-        X.drop(index = X.index[:n], axis = 0, inplace = True)
-        y = X[X.columns[-1]]
-        X.drop([X.columns[-1]], axis = 1, inplace = True)   # remove labels
+        X.replace(0.00, 1, inplace = True)          # replace 0 values
+        X.replace(0, 1, inplace = True)             # replace 0 values
+        X.replace('?', 1, inplace = True)             # replace 0 values
+        X.replace('y', 1, inplace = True)             # replace y values
+        X.replace('n', 0, inplace = True)             # replace n values
+        X.replace('democrat', 1, inplace = True)    # replace dem values
+        X.replace('republican', 0, inplace = True)  # replace rep values
+        X = X.sample(frac = 1)                                      # shuffle the data
+        n = round(int(len(X) * .50))                                # get size of training dataset
+        X.drop(index = X.index[:n], axis = 0, inplace = True)       # cull specified data
+        y = X[X.columns[-1]]                                        # move labels to y
+        X.drop([X.columns[-1]], axis = 1, inplace = True)     # remove labels from X
 
 
         print(X)
